22739477a8b2b963837ff60b4c64e75a0dc236af
hpsuenaa <cameron10913@gmail.com>
2016-11-28 00:22:54 +0800

    Removed social sharing

--- app/js_controllers/eventInfo.controller.js
+++ app/js_controllers/eventInfo.controller.js
@@ -47,32 +47,4 @@ angular.module("thehonorclub")
 
   console.log($scope.event);
 
-
-  // Social Sharing
-  var message = "Hey, checkout this event" + event.name + "."; 
-
-  $scope.shareTwitter = function() {
-    /*$cordovaSocialSharing.canShareVia("twitter", message).then(function(result) {
-      $cordovaSocialSharing.shareViaTwitter(message);
-    }, function(error) { alert("Cannot share on Twitter");});*/
-  };
-
-  $scope.shareInstagram = function() {
-    // $cordovaSocialSharing.canShareVia("instagram", message).then(function(result) {
-    //   $cordovaSocialSharing.shareViaInstagram(message);
-    // }, function(error) { alert("Cannot share on Instagram");});
-  };
-
-  $scope.shareWhatsApp = function() {
-    // $cordovaSocialSharing.canShareVia("whatsapp", message).then(function(result) {
-    //   $cordovaSocialSharing.shareViaWhatsApp(message);
-    // }, function(error) { alert("Cannot share on WhatsApp");});
-  };
-  
-  $scope.shareFacebook = function() {
-    // $cordovaSocialSharing.canShareVia("facebook", message).then(function(result) {
-    //   $cordovaSocialSharing.shareViaFacebook(message);
-    // }, function(error) { alert("Cannot share on Facebook");});
-  };
-
 }]);
